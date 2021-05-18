7dd76eb43d9fa52f5c05a257cafba0b7e7991324
Renato Daltiba Schneider <renatodaltiba@gmail.com>
2021-05-18 17:02:21 -0300

    Adicionando Header a aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>Dashgo</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
